8ede7d793ad4a4b8659d8231e89f3d90456aa37f
Alisher <alishermail111@gmail.com>
2024-02-24 12:29:13 +0600

    initlzd project

---
+++ src/main/java/com/example/demo/EmployeesDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesDataAppApplication.class, args);
+	}
+
+}
